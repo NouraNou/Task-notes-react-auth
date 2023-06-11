35dd55caafac55e254b967fc660c857e48ff8a74
[Alkandari-Y] <[yayalkandari@gmail.com]>
2023-06-11 18:06:42 +0300

    updated readme.md

--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  //   baseURL: "https://task-react-auth.herokuapp.com/api",
-  baseURL: "http://localhost:5000/api",
-});
-
-axios.interceptors.request.use((config) => {
-  const token = localStorage.get("token");
-  if (token) {
-    config.headers.Authorization = token;
-  }
-  return config;
+  baseURL: "https://task-react-auth.herokuapp.com/api",
+  // baseURL: "http://localhost:5000/api",
 });
 
 export default instance;
